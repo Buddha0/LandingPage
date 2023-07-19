0ff9a086b6171970ea58d1f9e2d9619bf704124e
Buddha <buddhalama88480@gmail.com>
2023-07-19 07:31:40 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector(".hamburger-menu")
+let ul = document.querySelector(".nav-ul")
+
+btn.addEventListener("click",function(){
+
+    ul.classList.toggle("show")
+
+
+})
